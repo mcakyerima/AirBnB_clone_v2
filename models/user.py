feacb5f316c93563948eb4f6d7771ab613adc80a
mcakyerima <mcakyerima@gmail.com>
2023-11-26 05:26:46 +0100

    fix

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 This is the Implementation of the User class which inherits from BaseModel
-and sqlAlchemy Base class
+and Base class of sqlAlchemy
 '''
 from os import getenv
 from sqlalchemy import Column, String
@@ -11,21 +11,10 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     '''
-    This is the Class Definition of the User class
+    Definition of the User class
     '''
     __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete, delete-orphan")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
