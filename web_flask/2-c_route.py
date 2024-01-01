135a417c19f6d775837c35ff00975b0fff42c4ed
mcakyerima <mcakyerima@gmail.com>
2024-01-01 22:06:40 +0100

    fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,10 +13,8 @@ This script starts a Flask web app.
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Display a greeting message.
@@ -24,7 +22,7 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Display "HBNB".
@@ -32,7 +30,7 @@ def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
     Display "C" followed by custom text.
@@ -41,4 +39,4 @@ def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,16 +7,14 @@ This script starts a Flask web app.
   - /hbnb: Display "HBNB"
   - /c/<text>: Display "C" + text (replace underscores with spaces)
   - /python: Display "Python is cool" (default text is "is cool")
-  - /python/<text>: Display "Python" + text (replace underscores with spaces, default is "is cool")
+  - /python/<text>: Display "Python" + text
+  - (replace underscores with spaces, default is "is cool")
 """
 
 from flask import Flask
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Display a greeting message.
@@ -24,7 +22,7 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Display "HBNB".
@@ -32,7 +30,7 @@ def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
     Display "C" followed by custom text.
@@ -41,8 +39,8 @@ def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python')
-@app.route('/python/<text>')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     """
     Display "Python" followed by custom text.
@@ -56,4 +54,4 @@ def python_text(text="is cool"):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
