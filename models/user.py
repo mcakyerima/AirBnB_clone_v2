3f7fd8b70342ec881cff575d3e8a23065c88a4d0
mcakyerima <mcakyerima@gmail.com>
2024-01-02 03:23:41 +0100

    fixed buggs and refactored classes

--- models/user.py
+++ models/user.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-'''
-    Implementation of the User class which inherits from BaseModel
-'''
-from os import getenv
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    '''
-        Definition of the User class
-    '''
-    __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete, delete-orphan")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
